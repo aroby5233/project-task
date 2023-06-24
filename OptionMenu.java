dffd6ab5f790ff39ff02d3de3e0056b2fd8fb8b8
aroby5233 <124565218+aroby5233@users.noreply.github.com>
2023-06-24 12:38:43 +0600

    Update OptionMenu.java

--- OptionMenu.java
+++ OptionMenu.java
@@ -1,1 +1,58 @@
-https://github.com/rajyash1904/ATM-Machine/tree/master/ATM
+
+				System.out.println("\nEnter your customer number ");
+				cst_no = menuInput.nextInt();
+				Iterator it = data.entrySet().iterator();
+				while (it.hasNext()) {
+					Map.Entry pair = (Map.Entry) it.next();
+					if (!data.containsKey(cst_no)) {
+						end = true;
+					}
+				}
+				if (!end) {
+					System.out.println("\nThis customer number is already registered");
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("\nInvalid Choice.");
+				menuInput.next();
+			}
+		}
+		System.out.println("\nEnter PIN to be registered");
+		int pin = menuInput.nextInt();
+		data.put(cst_no, new Account(cst_no, pin));
+		System.out.println("\nYour new account has been successfuly registered!");
+		System.out.println("\nRedirecting to login.............");
+		getLogin();
+	}
+
+	public void mainMenu() throws IOException {
+		data.put(952141, new Account(952141, 191904, 1000, 5000));
+		data.put(123, new Account(123, 123, 20000, 50000));
+		boolean end = false;
+		while (!end) {
+			try {
+				System.out.println("\n Type 1 - Login");
+				System.out.println(" Type 2 - Create Account");
+				System.out.print("\nChoice: ");
+				int choice = menuInput.nextInt();
+				switch (choice) {
+				case 1:
+					getLogin();
+					end = true;
+					break;
+				case 2:
+					createAccount();
+					end = true;
+					break;
+				default:
+					System.out.println("\nInvalid Choice.");
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("\nInvalid Choice.");
+				menuInput.next();
+			}
+		}
+		System.out.println("\nThank You for using this ATM.\n");
+		menuInput.close();
+		System.exit(0);
+	}
+}
